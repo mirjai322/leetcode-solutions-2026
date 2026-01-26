abddbf1301bca0ba5ec0a72a4c17e3953f341582
mirayajain <mirayajain@gmail.com>
2026-01-25 20:32:11 -0600

    added minimum number game soln

--- solutions.py
+++ solutions.py
@@ -35,3 +35,15 @@ class Solution:
                 return [hashmap[target - num], i]
             else:
                 hashmap[num] = i
+
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        arr = []
+        nums.sort()
+        for i in range(0, len(nums), 2):
+            a = nums[i]
+            b = nums[i + 1]
+            arr.append(b)
+            arr.append(a)
+        return arr
+
