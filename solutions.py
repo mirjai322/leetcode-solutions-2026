cbc2ac2fb06c3b63d3e3ba04a4a68a8cc61fcd21
mirayajain <mirayajain@gmail.com>
2026-01-28 11:37:35 -0600

    added some solutions for stack, 2 pointers, LL, heap

--- solutions.py
+++ solutions.py
@@ -47,3 +47,66 @@ class Solution:
             arr.append(a)
         return arr
 
+
+################### STACK ###################
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        close_to_open = {"}": "{", ")": "(", "]": "["}
+        for char in s:
+            if char == "{" or char == "[" or char == "(":
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                opened = stack.pop()
+                if close_to_open[char] != opened:
+                    return False
+        if not stack:
+            return True
+        return False
+
+
+################### 2 POINTERS ###################
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        max_so_far = 0
+        l, r = 0, len(heights) - 1
+        while l < r:
+            current = (r - l) * min(heights[l], heights[r])
+            max_so_far = max(max_so_far, current)
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_so_far
+    
+
+################### LINKED LIST ###################
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True
+        return False
+
+
+################### HEAP ###################
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = [num for num in nums]
+        heapq.heapify(self.heap)
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+        self.localvar = k
+        
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.localvar:
+            heapq.heappop(self.heap)
+        return self.heap[0]
